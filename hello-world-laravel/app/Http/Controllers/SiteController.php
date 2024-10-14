afb4e7b3a2b903c783185966f279e6b62ca1cec1
Coutinhopmw <cassiocoutinho258@gmail.com>
2024-10-14 19:53:30 -0300

    Exercício 01: Components.

--- hello-world-laravel/app/Http/Controllers/SiteController.php
+++ hello-world-laravel/app/Http/Controllers/SiteController.php
@@ -7,14 +7,31 @@ use Illuminate\Http\Request;
 class SiteController extends Controller
 {
     public function index() {
-        $data = [
-            'ingredientes' => [
-                'Farinha',
-                'ovos',
-                'Farinha 2',
-                'ovos 2'
+        $pessoas = [
+            [
+                'image' => 'https://i.pravatar.cc/150?img='.rand(0,50),
+                'nome' => 'Alessandro',
+                'birth' => '29/01/90',
+                'idade' => 29,  
+            ],
+
+            [
+                'image' => 'https://i.pravatar.cc/150?img='.rand(0,50),
+                'nome' => 'José',
+                'birth' => '20/08/76',
+                'idade' => 29,  
+            ],
+
+            [
+                'image' => 'https://i.pravatar.cc/150?img='.rand(0,50),
+                'nome' => 'Maria',
+                'birth' => '10/04/99',
+                'idade' => 29,  
             ]
         ];
-        return view('bemvindo',$data);
+
+        $dados['pessoas'] = $pessoas;
+
+        return view('bemvindo',$dados);
     }
 }
--- hello-world-laravel/resources/views/bemvindo.blade.php
+++ hello-world-laravel/resources/views/bemvindo.blade.php
@@ -6,42 +6,23 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Bem vindo</title>
 </head>
-<body>
-
-    {{-- @if ($nome = 'Cássio')
-        <h1>Sim</h1>
-    @else 
-        <h2>Não</h2>
-    @endif
-
-    @for ($i = 0; $i <= 10; $i++)
-        <p> Valor de i é: {{$i}} </p>
-    @endfor --}}
-
-    @foreach ($ingredientes as $ing)
-
-        <p> 
-            {{$ing}}
-        
-            @component('components.botao')
-                @slot('cor')
-                    blue
-                @endslot
-            Editar
-            @endcomponent
-
-            @component('components.botao')
-                @slot('cor')
-                    red
-                @endslot
-            Deletar
-            @endcomponent
-
-        </p>
-        
-    @endforeach
+<body style="margin:0;">
 
 
+    <div class="" style="display:flex; flex-direction: row; width:100%; height:100vh; background-color:rgb(247, 247, 247); margin:0;">
+        @foreach($pessoas as $ing)
+            <div class="" style="width: 25vh; height:40vh; background-color:rgb(235, 235, 235); border-radius:2%; margin:10vh;">
+                <div class="">
+                    <img style="width:90% !important; margin:5%;" src=" {{$ing['image']}}" alt="">
+                </div>
+                <div class="">
+                    <p style="text-align: center; font-size:x-large; font-weight:bold;">{{$ing['nome']}}</p>
+                    <p style="text-align: center; font-size:large;">Idade:{{$ing['idade']}} anos</p>
+                    <p style="text-align: center; font-size:large;"> Data nascimento:{{$ing['birth']}}</p>
+                </div>
+            </div>
+        @endforeach
+    </div>
 
 </body>
 
